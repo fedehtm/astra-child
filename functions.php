3b4b9a47623924bbb218f8a451b7422c917427dc
Federico <25438748+fedehtm@users.noreply.github.com>
2019-04-04 10:10:43 -0300

    PAgina rota
    add_filter( 'gettext', 'translate_woocommerce_strings', 999, 3 );
    function translate_woocommerce_strings( $translated, $text, $domain ) {
    	if(is_page('carrito')) {
    		$translated = str_ireplace( 'Total del carrito!', 'Elegir metodo de pago', $translated );
    		$translated = str_ireplace( 'Finalizar compra', 'Pagar', $translated );	
    		return $translated;
    	}
    }

--- functions.php
+++ functions.php
@@ -464,12 +464,4 @@ if(is_page('carrito')) {  ?>
 <?php }
 };
 
-add_filter( 'gettext', 'translate_woocommerce_strings', 999, 3 );
-function translate_woocommerce_strings( $translated, $text, $domain ) {
-	if(is_page('carrito')) {
-		$translated = str_ireplace( 'Total del carrito!', 'Elegir metodo de pago', $translated );
-		$translated = str_ireplace( 'Finalizar compra', 'Pagar', $translated );	
-		return $translated;
-	}
-}
 ?>
